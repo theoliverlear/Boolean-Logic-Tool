0fbb9003360f055431343b4a216e91d9b1bdf739
theoliverlear <sigwartholiver2@gmail.com>
2024-02-29 15:16:21 -0600

    Add size and variable symbol tests

--- src/test/java/VariableClusterTest.java
+++ src/test/java/VariableClusterTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VariableClusterTest {
+    Variable variableA = new Variable('A');
+    Variable variableB = new Variable('B', true);
+    VariableCluster testVariableCluster = new VariableCluster(variableA, variableB);
+    @Test
+    public void testSize() {
+        assertEquals(2, this.testVariableCluster.size());
+    }
+    @Test
+    public void testGetVariableSymbols() {
+        ArrayList<Character> variableSymbols = new ArrayList<>();
+        variableSymbols.add('A');
+        variableSymbols.add('B');
+        assertEquals(variableSymbols, this.testVariableCluster.getVariableSymbols());
+    }
 }
