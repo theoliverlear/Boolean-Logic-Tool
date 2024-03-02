9b226889c9f900efc872bab13fc701347b80219e
theoliverlear <sigwartholiver2@gmail.com>
2024-03-01 19:51:34 -0600

    Fix parenthesis bug and add comments

--- src/main/java/FormulaParser.java
+++ src/main/java/FormulaParser.java
@@ -1,14 +1,24 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class FormulaParser {
     String formula;
     int numVariables;
+    boolean isPrime = false;
     ArrayList<FormulaParser> subFormulas = new ArrayList<>();
     ArrayList<VariableCluster> variableClusters = new ArrayList<>();
     TruthTable truthTable;
     public FormulaParser(String formula) {
         this.formula = formula.toUpperCase().replaceAll(" ", "");
+        this.numVariables = this.formulaVariableCount();
+        this.truthTable = new TruthTable(this.numVariables);
+    }
+    public FormulaParser(String formula, boolean isPrime) {
+        this.formula = formula.toUpperCase().replaceAll(" ", "");
+        this.isPrime = isPrime;
         this.numVariables = this.formulaVariableCount();
         this.truthTable = new TruthTable(this.numVariables);
     }
@@ -24,44 +34,6 @@ public class FormulaParser {
             }
         }
         return variableSymbols.size();
-    }
-    public BinaryFunction getBinaryFunction() {
-        String[] formulaOrArray = this.formula.split("\\+");
-        if (formulaOrArray.length > 1) {
-            System.out.println("OR");
-            return BinaryFunction.OR;
-        }
-        String[] formulaNotArray = this.formula.split("'");
-        if (formulaNotArray.length == 1 && formulaNotArray[0].length() == 1) {
-            System.out.println("NOT");
-            return BinaryFunction.NOT;
-        }
-        System.out.println("AND");
-        return BinaryFunction.AND;
-        // TODO: If after a split, the size is greater than one for the length
-        //       of the string, then we recursively call to get another binary
-        //       function. We continuously apply formulas until we have a set
-        //       of binary digits to put into a cluster or compare with Binary
-        //       object methods.
-    }
-    public Binary getFormulaBinary(Binary binaryDigit, Binary comparedBinaryDigit) {
-        BinaryFunction binaryFunction = this.getBinaryFunction();
-        Binary formulaOutput = switch (binaryFunction.getFunction()) {
-            case "AND" -> {
-                System.out.println("Executing AND");
-                yield binaryDigit.and(comparedBinaryDigit);
-            }
-            case "OR" -> {
-                System.out.println("Executing OR");
-                yield binaryDigit.or(comparedBinaryDigit);
-            }
-            case "NOT" -> {
-                System.out.println("Executing NOT");
-                yield binaryDigit.not();
-            }
-            default -> throw new IllegalStateException("Unexpected value: " + binaryFunction.getFunction());
-        };
-        return formulaOutput;
     }
     public ArrayList<Variable> parseVariables(String formulaSubstring) {
         ArrayList<Variable> variables = new ArrayList<>();
@@ -89,14 +61,27 @@ public class FormulaParser {
         String[] formulaOrArray = this.formula.split("\\+");
         ArrayList<String> variableSymbols = new ArrayList<>(Arrays.asList(formulaOrArray));
         ArrayList<String> innerFormulaStrings = new ArrayList<>();
+        ArrayList<HashMap<String, Boolean>> innerFormulaWithPrimeMap = new ArrayList<>();
         for (int i = 0; i < variableSymbols.size(); i++) {
             String variableSymbol = variableSymbols.get(i);
             if (variableSymbol.contains("(") && !variableSymbol.contains(")")) {
                 for (int j = i + 1; j < variableSymbols.size(); j++) {
                     String innerVariableSymbol = variableSymbols.get(j);
-                    if (innerVariableSymbol.contains(")")) {
-                        innerFormulaStrings.add(variableSymbol.replace("(", "")
-                                + "+" + innerVariableSymbol.replace(")", ""));
+                    if (innerVariableSymbol.contains(")'")) {
+                        String withoutPrimeBrackets = variableSymbol.replace("(", "")
+                                + "+" + innerVariableSymbol.replace(")'", "");
+                        HashMap<String, Boolean> innerFormulaMap = new HashMap<>();
+                        innerFormulaMap.put(withoutPrimeBrackets, true);
+                        innerFormulaWithPrimeMap.add(innerFormulaMap);
+                        variableSymbols.remove(j);
+                        variableSymbols.remove(i);
+                        break;
+                    } else if (innerVariableSymbol.contains(")")) {
+                        String withoutPrimeBrackets = variableSymbol.replace("(", "")
+                                + "+" + innerVariableSymbol.replace(")", "");
+                        HashMap<String, Boolean> innerFormulaMap = new HashMap<>();
+                        innerFormulaMap.put(withoutPrimeBrackets, false);
+                        innerFormulaWithPrimeMap.add(innerFormulaMap);
                         variableSymbols.remove(j);
                         variableSymbols.remove(i);
                         break;
@@ -104,18 +89,19 @@ public class FormulaParser {
                 }
             }
         }
-        for (String innerFormulaString : innerFormulaStrings) {
-            this.subFormulas.add(new FormulaParser(innerFormulaString));
+        for (HashMap<String, Boolean> innerFormulaMap : innerFormulaWithPrimeMap) {
+            for (Map.Entry<String, Boolean> entry : innerFormulaMap.entrySet()) {
+                String innerFormulaString = entry.getKey();
+                boolean isPrime = entry.getValue();
+                this.subFormulas.add(new FormulaParser(innerFormulaString, isPrime));
+            }
         }
         this.subFormulas.forEach(FormulaParser::findCluster);
-
-
         ArrayList<ArrayList<Variable>> splitVariableClusters = new ArrayList<>();
         for (String variableStringCluster : variableSymbols) {
             splitVariableClusters.add(this.parseVariables(variableStringCluster));
         }
         System.out.println(splitVariableClusters);
-
         ArrayList<VariableCluster> variableClusters = new ArrayList<>();
         for (ArrayList<Variable> variableCluster : splitVariableClusters) {
             boolean isPrime = false;
@@ -155,77 +141,89 @@ public class FormulaParser {
     }
     public ArrayList<Binary> getFullFormulaBinary() {
         ArrayList<BinaryRow> binaryRows = this.truthTable.getTruthTableRows();
-        ArrayList<BinaryColumn> binaryColumns = this.truthTable.getTruthTableColumns();
-        ArrayList<Binary> finalBinaryOrDigits = new ArrayList<>();
-        ArrayList<ArrayList<VariableCluster>> allVariableClusterList = new ArrayList<>();
-        allVariableClusterList.add(this.variableClusters);
-        for (FormulaParser subFormula : this.subFormulas) {
-            allVariableClusterList.add(subFormula.variableClusters);
-        }
-        for (BinaryRow binaryRow : binaryRows) {
-            ArrayList<Binary> variableClusterBinaryDigits = new ArrayList<>();
-            for (ArrayList<VariableCluster> variableClusterList : allVariableClusterList) {
+        ArrayList<Binary> finalBinaryDigits = new ArrayList<>();
+        ArrayList<FormulaParser> formulas = new ArrayList<>();
+        formulas.add(this);
+        formulas.addAll(this.subFormulas);
 
-                for (VariableCluster variableCluster : variableClusterList) {
+        /*
+        - Final output
+        - Formula - OR
+        - Variable cluster - AND
+        - Variable - AND
+         */
+
+        // Loop through each row. This will be the final formula combination
+        // output.
+        for (BinaryRow binaryRow : binaryRows) { // Look for final output of each row
+            ArrayList<Binary> formulasOutputDigits = new ArrayList<>();
+            // Loop through each formula. This will be the OR'd with the
+            // output of other formulas.
+            for (FormulaParser formula : formulas) { // Look for final output of each formula
+                ArrayList<VariableCluster> clustersInFormula = formula.variableClusters;
+                ArrayList<Binary> formulaClusters = new ArrayList<>();
+                // For each cluster in the formula, we will get the output
+                // of the cluster and then AND its contents.
+                for (VariableCluster variableCluster : clustersInFormula) { // Look for final output of each cluster
                     ArrayList<Character> variableSymbols = variableCluster.getVariableSymbols();
                     char[] binaryDigitsOrder = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
                     ArrayList<Binary> binaryDigits = binaryRow.getBinaryRow();
                     char[] binaryDigitsOrderSubset = Arrays.copyOf(binaryDigitsOrder, this.numVariables);
-                    char[] finalBinaryVariableOrder = new char[this.numVariables];
-//                    for (int i = binaryDigitsOrderSubset.length - 1, j = 0; i >= 0 && j < binaryDigitsOrderSubset.length; i--, j++) {
-//                        finalBinaryVariableOrder[j] = binaryDigitsOrderSubset[i];
-//                    }
-                    for (int i = 0; i < binaryDigitsOrderSubset.length; i++) {
-                        finalBinaryVariableOrder[i] = binaryDigitsOrderSubset[i];
-                    }
                     ArrayList<Binary> binaryClusterDigits = new ArrayList<>();
-                    for (int i = 0; i < variableSymbols.size(); i++) {
-                        char variableSymbol = variableSymbols.get(i);
-                        for (Variable variable : variableCluster.getVariableCluster()) {
+                    for (char variableSymbol : variableSymbols) {
+                        // Loop through each variable in the cluster and get
+                        // the binary digit from the input. We will then AND
+                        // the binary digits together.
+                        ArrayList<Variable> variableInsideCluster = variableCluster.getVariableCluster();
+                        for (Variable variable : variableInsideCluster) { // Look for final output of each variable
                             if (variable.getVariableSymbol() == variableSymbol) {
                                 int variableIndex;
-                                for (variableIndex = 0; variableIndex < finalBinaryVariableOrder.length; variableIndex++) {
-                                    if (finalBinaryVariableOrder[variableIndex] == variableSymbol) {
+                                for (variableIndex = 0; variableIndex < binaryDigitsOrderSubset.length; variableIndex++) {
+                                    if (binaryDigitsOrderSubset[variableIndex] == variableSymbol) {
                                         break;
                                     }
                                 }
-                                if (variableIndex == finalBinaryVariableOrder.length) {
+                                if (variableIndex == binaryDigitsOrderSubset.length) {
                                     continue;
                                 }
                                 Binary binaryDigit = binaryDigits.get(variableIndex);
                                 binaryClusterDigits.add(variable.getBinaryFromInput(binaryDigit));
                             }
-                        }
+                        } // All variables in the cluster found
                     }
                     BinaryRow binaryClusterRow = new BinaryRow(binaryClusterDigits);
                     Binary binaryClusterOutput = binaryClusterRow.rowAnd();
                     if (variableCluster.getIsPrime()) {
                         binaryClusterOutput = binaryClusterOutput.not();
                     }
-                    variableClusterBinaryDigits.add(binaryClusterOutput);
+                    formulaClusters.add(binaryClusterOutput);
+                } // All clusters binary digits found
+                BinaryRow variableClustersBinaryRow = new BinaryRow(formulaClusters);
+                Binary variableClustersBinaryOutput = variableClustersBinaryRow.rowOr();
+                if (formula.isPrime) {
+                    variableClustersBinaryOutput = variableClustersBinaryOutput.not();
                 }
-            }
-            BinaryRow variableClusterBinaryRow = new BinaryRow(variableClusterBinaryDigits);
-            Binary variableClusterBinaryOutput = variableClusterBinaryRow.rowOr();
-            finalBinaryOrDigits.add(variableClusterBinaryOutput);
-        }
-        BinaryColumn finalBinaryColumn = new BinaryColumn(finalBinaryOrDigits);
+                formulasOutputDigits.add(variableClustersBinaryOutput);
+            } // All formula binary digits found
+            BinaryRow formulaRow = new BinaryRow(formulasOutputDigits);
+            Binary formulaRowOutput = formulaRow.rowOr();
+            finalBinaryDigits.add(formulaRowOutput);
+        } // All function binary digits found
+        BinaryColumn finalBinaryColumn = new BinaryColumn(finalBinaryDigits);
         TruthTable finalTruthTable = new TruthTable(this.numVariables, finalBinaryColumn);
         System.out.println(finalTruthTable);
-        return finalBinaryOrDigits;
+        return finalBinaryDigits;
+    }
+    public void simplifyFormula() {
+
     }
     @Override
     public String toString() {
+
         return this.formula;
     }
     public static void main(String[] args) {
-//        FormulaParser formulaParser = new FormulaParser("A'");
-//        System.out.println(formulaParser.parsePrimeVariable());
-//        Binary binaryDigit = Binary.ONE;
-//        Binary comparedBinaryDigit = Binary.ZERO;
-//        System.out.println(formulaParser.getFormulaBinary(binaryDigit, comparedBinaryDigit));
-//        FormulaParser formulaParser = new FormulaParser("(A'C+B)'+(CB')'+A'B'C'");
-        FormulaParser formulaParser = new FormulaParser("AB+C");
+        FormulaParser formulaParser = new FormulaParser("AB+CD");
         formulaParser.findCluster();
         System.out.println(formulaParser);
         System.out.println(formulaParser.getFullFormulaBinary());
