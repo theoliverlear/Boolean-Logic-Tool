22183340d1aa8ad8075f12f47e766a7b4db88905
theoliverlear <sigwartholiver2@gmail.com>
2024-07-19 14:06:36 -0500

    Add webpack.config.js for bundling JavaScript and TypeScript scripts, setting environment, and loading modules

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        // Scripts
+        './src/main/resources/static/script/globalScript.ts',
+        './src/main/resources/static/script/homeScript.ts',
+
+        // Models
+
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-typescript'
+                            ]
+                        }
+                    },
+                    'ts-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js'
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './src/main/resources/static/script'),
+    },
+    mode: 'development',
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+};
