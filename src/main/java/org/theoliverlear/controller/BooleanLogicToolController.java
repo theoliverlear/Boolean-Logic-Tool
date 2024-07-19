d984a95023550751587ae36d4ad2414bc64a6a90
theoliverlear <sigwartholiver2@gmail.com>
2024-07-19 14:10:52 -0500

    Add formula endpoint to handle FormulaRequest and return TruthTable in BooleanLogicToolController.java

--- src/main/java/org/theoliverlear/controller/BooleanLogicToolController.java
+++ src/main/java/org/theoliverlear/controller/BooleanLogicToolController.java
@@ -1,12 +1,23 @@
 package org.theoliverlear.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.comm.request.FormulaRequest;
+import org.theoliverlear.model.BinaryFormula;
+import org.theoliverlear.model.TruthTable;
 
 @Controller
 public class BooleanLogicToolController {
     @RequestMapping("/")
     public String home() {
         return "home";
+    }
+    @RequestMapping("/formula")
+    public ResponseEntity<TruthTable> formula(@RequestBody FormulaRequest formulaRequest) {
+        BinaryFormula formula = new BinaryFormula(formulaRequest.getFormula());
+        return new ResponseEntity<>(formula.getTruthTable(), HttpStatus.OK);
     }
 }
