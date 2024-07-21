d1fdeb9310d8f96bd6d4b834ea048f4eb87b2342
theoliverlear <sigwartholiver2@gmail.com>
2024-07-21 16:45:05 -0500

    Refactor homeScript.ts to import TruthTable model, add new types, and add functionality to add truth tables to the page

--- src/main/resources/static/script/homeScript.ts
+++ src/main/resources/static/script/homeScript.ts
@@ -1,6 +1,32 @@
+import {TruthTable} from "./models/TruthTable";
+
 const functionInput: JQuery<HTMLElement> = $("#function-input");
 const submitButton: JQuery<HTMLElement> = $("#submit-button");
 const truthTableDiv: JQuery<HTMLElement> = $("#truth-table-div");
+const truthTableSection: JQuery<HTMLElement> = $("#truth-table-section");
+
+type SimpleBinary = {
+    binary: string
+}
+
+type SimpleTableColumn = {
+    numVariables: number,
+    positionFromLast: number,
+    binaryColumn: SimpleBinary[]
+}
+
+type SimpleTableRow = {
+    binaryRow: SimpleBinary[]
+}
+
+export type SimpleTruthTable = {
+    truthTable: {
+        numVariables: number,
+        truthTableColumns: SimpleTableColumn[],
+        truthTableRows: SimpleTableRow[],
+        functionColumn: SimpleTableColumn
+    }
+}
 
 function sendFormulaToServer() {
     console.log('functionInput: ', functionInput.val() as string);
@@ -12,15 +38,21 @@ function sendFormulaToServer() {
         body: JSON.stringify({
             formula: functionInput.val() as string
         })
-    }).then((response: Response) => response.json()).then(responseJson => {
+    }).then((response: Response) => {
+        console.log('response: ', response);
+        loadTruthTableToPage(response);
+    }).then(responseJson => {
         console.log(responseJson);
-        let truthTable: string = responseJson.truthTable;
-        truthTableDiv.text(truthTable);
     }).catch(error => {
         console.error('Error: ', error);
     });
 }
-function loadTruthTableToPage() {
-
+function loadTruthTableToPage(truthTableResponse: Response): void {
+    console.log('truthTableResponse: ', truthTableResponse);
+    truthTableResponse.json().then((simpleTruthTable: SimpleTruthTable) => {
+        let truthTable: TruthTable = new TruthTable(simpleTruthTable);
+        truthTableSection.empty();
+        truthTableSection.append(truthTable.getHtml());
+    });
 }
 submitButton.on('click', sendFormulaToServer);
