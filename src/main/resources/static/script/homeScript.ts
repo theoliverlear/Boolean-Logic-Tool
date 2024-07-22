91e62e059195b01059d53888f1e71cf823703362
theoliverlear <sigwartholiver2@gmail.com>
2024-07-22 14:03:48 -0500

    Refactor homeScript.ts for better function input handling and add updateFormulaTitle function

--- src/main/resources/static/script/homeScript.ts
+++ src/main/resources/static/script/homeScript.ts
@@ -2,23 +2,20 @@ import {TruthTable} from "./models/TruthTable";
 
 const functionInput: JQuery<HTMLElement> = $("#function-input");
 const submitButton: JQuery<HTMLElement> = $("#submit-button");
-const truthTableDiv: JQuery<HTMLElement> = $("#truth-table-div");
 const truthTableSection: JQuery<HTMLElement> = $("#truth-table-section");
+const formulaTitleText: JQuery<HTMLElement> = $("#formula-title-text");
 
 type SimpleBinary = {
     binary: string
 }
-
 type SimpleTableColumn = {
     numVariables: number,
     positionFromLast: number,
     binaryColumn: SimpleBinary[]
 }
-
 type SimpleTableRow = {
     binaryRow: SimpleBinary[]
 }
-
 export type SimpleTruthTable = {
     truthTable: {
         numVariables: number,
@@ -27,8 +24,24 @@ export type SimpleTruthTable = {
         functionColumn: SimpleTableColumn
     }
 }
+function updateFormulaTitle(): void {
+    formulaTitleText.text(functionInput.val() as string);
+}
 
-function sendFormulaToServer() {
+function formulaSequence(event: JQuery.KeyDownEvent): void {
+    let invalidKeyDown: boolean;
+    let keyValue: string = String.fromCharCode(event.which);
+    invalidKeyDown = /[\d.]/.test(keyValue) || keyValue === ' ';
+    if (invalidKeyDown) {
+        event.preventDefault();
+    }
+    if (functionInput.val() === '') {
+        formulaTitleText.text('Enter a formula');
+    } else {
+        updateFormulaTitle();
+    }
+}
+function sendFormulaToServer(): void {
     console.log('functionInput: ', functionInput.val() as string);
     fetch('/formula', {
         method: 'POST',
@@ -38,10 +51,10 @@ function sendFormulaToServer() {
         body: JSON.stringify({
             formula: functionInput.val() as string
         })
-    }).then((response: Response) => {
+    }).then((response: Response): void => {
         console.log('response: ', response);
         loadTruthTableToPage(response);
-    }).then(responseJson => {
+    }).then((responseJson: void): void => {
         console.log(responseJson);
     }).catch(error => {
         console.error('Error: ', error);
@@ -49,10 +62,11 @@ function sendFormulaToServer() {
 }
 function loadTruthTableToPage(truthTableResponse: Response): void {
     console.log('truthTableResponse: ', truthTableResponse);
-    truthTableResponse.json().then((simpleTruthTable: SimpleTruthTable) => {
+    truthTableResponse.json().then((simpleTruthTable: SimpleTruthTable): void => {
         let truthTable: TruthTable = new TruthTable(simpleTruthTable);
         truthTableSection.empty();
         truthTableSection.append(truthTable.getHtml());
     });
 }
 submitButton.on('click', sendFormulaToServer);
+functionInput.on('keydown', formulaSequence);
